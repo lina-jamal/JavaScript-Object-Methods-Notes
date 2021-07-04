cac1db924531cbf8c759f1503d05784d69cbb56f
lina-jamal <linahjamal89@gmail.com>
2021-07-04 22:48:57 +0300

    add example of object method

---
+++ script.js
@@ -0,0 +1,27 @@
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+// };
+// console.log(person.firstName);
+// console.log(person["firstName"]);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName: function () {
+//     return person.firstName + " " + person.lastName;
+//   },
+// };
+
+///How access an object method ???
+///objectName.methodName();
+// console.log(person.fullName());
+/// i can also store the result in variable name
+// let name = person.fullName()
+
+// const addOne = {
+
+// };
+// console.log(addOne.sum());
